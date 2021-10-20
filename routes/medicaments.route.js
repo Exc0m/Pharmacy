cdca8a526770a1f9b6bbbb15d795131b864d1fb0
unknown <visingiriev@mail.ru>
2021-10-20 12:02:51 +0300

    Добавил CRUD/Добавил роуты

---
+++ routes/medicaments.route.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const { medicamentsController } = require("../controllers/medicaments.controller");
+
+const router = Router();
+
+router.post("/admin/add", medicamentsController.addMedicament); // Добавление лекарства админом
+router.get("/", medicamentsController.getAllMedicament); // Вывод всех лекарств
+router.get("/id", medicamentsController.getMedicament); // Вывод лекарства по ID
+router.patch("/admin/:id", medicamentsController.patchMedicament); // Изменение лекарства
+router.delete("/admin/:id", medicamentsController.deleteMedicament); // Удаление лекарства
+
+module.exports = router;
