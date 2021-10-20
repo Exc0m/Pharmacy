f0f63ff38e2b5b930faca5f563e6c0506b5b70c8
unknown <visingiriev@mail.ru>
2021-10-20 12:20:19 +0300

    Добавил в модель Категории

--- models/Medicament.model.js
+++ models/Medicament.model.js
@@ -4,6 +4,10 @@ const medicamentSchema = mongoose.Schema({
   name: String,
   price: Number,
   needRecipe: Boolean,
+  category: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "Category",
+  },
 });
 
 const Medicament = mongoose.model("Medicament", medicamentSchema);
