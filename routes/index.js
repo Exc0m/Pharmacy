8e39dc4e03f237576cbcadd1a5913fdf6b91b77e
Atamazow <atamazov99@mail.ru>
2021-10-20 11:37:06 +0300

    Прописал пути в routes

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,10 @@ const { Router } = require("express");
 
 const router = Router();
 
+router.use('/categories',require('./categories.route'))
+router.use('/medicaments',require('./medicaments.route'))
+router.use('/cart',require('./cart.route'))
+router.use('/users',require('./users.route'))
+
+
 module.exports = router;
